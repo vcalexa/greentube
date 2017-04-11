a32e01c1697a32335f90719f3239b9b650ab9269
vcalexa <dalimagic@yahoo.com>
2017-04-11 19:55:57 +0300

    finished initial tests

--- conf.js
+++ conf.js
@@ -1,10 +1,27 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
+  
+  multiCapabilities:[ 
+  
+  {
+    browserName: 'chrome',
+	'chromeOptions':{
+		
+	//to allows notificatons	
+	prefs:{
+		
+		'profile.managed_default_content_settings.notifications':1,
+		'profile': {
+                  'password_manager_enabled': false
+              }
+	}
+	
+	}
+	
+  }],
   suites: {
-  Createnew:'test/create.js',
-  Update:'update.js',
-  Delete:'delete.js',
-  Read:'read.js'
+  Createnew:'test/login.js',
+  //Navigation:'test/navigation.js'
   }
   
 };
